0278cac9b0747a00478f847d4baa7d91f15f5747
Abigail Carey <abigailcarey123@gmail.com>
2017-04-28 15:24:05 -0700

    Adding model

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ window.addEventListener('DOMContentLoaded', function () {
     var scene = createScene();
 
     // Load the dinosaur model
-    BABYLON.SceneLoader.ImportMesh("Dino", "models/", "dino.babylon", scene, function (newMeshes) {
+    BABYLON.SceneLoader.ImportMesh("Dino", "models/", "dino2.babylon", scene, function (newMeshes) {
 
 
         dino = newMeshes[0];
