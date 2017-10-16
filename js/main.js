843c3fe68e4512c55a27a1027d2f508a98eae787
Abby Carey <abigailc@microsoft.com>
2017-10-16 14:00:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,7 +76,6 @@ window.addEventListener('DOMContentLoaded', function () {
         // Create the Babylon scene
         var scene = new BABYLON.Scene(engine);
 	
-	scene.createDefaultVRExperience();
         // Apply gravity so that any Y axis movement is ignored
         scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
 
